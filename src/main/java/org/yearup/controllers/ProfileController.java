7bafd425292d98fbbdd48eba7ad537971d4cf83d
gmanyani527 <131324621+gmanyani527@users.noreply.github.com>
2025-06-26 09:40:22 -0400

    Add GET endpoint to fetch user profile by username

--- src/main/java/org/yearup/controllers/ProfileController.java
+++ src/main/java/org/yearup/controllers/ProfileController.java
@@ -1,9 +1,12 @@
 package org.yearup.controllers;
 
+import org.springframework.security.core.Authentication;
 import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import org.yearup.data.ProfileDao;
+import org.yearup.models.Profile;
 
 @RestController
 @RequestMapping("/profile")
@@ -13,5 +16,10 @@ public class ProfileController {
 
     public ProfileController(ProfileDao profileDao) {
         this.profileDao = profileDao;
+    }
+    @GetMapping
+    public Profile getProfile(Authentication auth) {
+        String username = auth.getName(); // comes from Spring Security
+        return profileDao.getByUserName(username);
     }
 }
--- src/main/java/org/yearup/data/ProfileDao.java
+++ src/main/java/org/yearup/data/ProfileDao.java
@@ -6,4 +6,6 @@ import org.yearup.models.Profile;
 public interface ProfileDao
 {
     Profile create(Profile profile);
+    Profile getByUserName(String username);
+    void update(String username, Profile profile);
 }
